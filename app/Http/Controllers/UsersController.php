d8ae9582b96f53aa4fbbfbfdc1d76b0313783b67
Asif Khan <asifkhan.github@gmail.com>
2019-01-02 22:16:05 +0600

    data fetched and passed to the view

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller {
+
+    //load home page with datas
+    public function index(){
+        $all_users=User::all();
+        return view('home',['users'=>$all_users]);
+    }
     
 }
