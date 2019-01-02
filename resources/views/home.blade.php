75651e6a88360b12ecb54e24acdd738786cacc2c
Asif Khan <asifkhan.github@gmail.com>
2019-01-03 01:55:36 +0600

    remove user confirmation modal added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,7 +34,7 @@
                                                     data-username="{{ $user->username }}" data-email="{{ $user->email }}" data-designation="{{ $user->designation }}">View</button>
                                             <button class="btn btn-raised btn-info" data-toggle="modal" id="editUserModal" data-target="#editModal" data-id="{{ $user->id }}"
                                                     data-username="{{ $user->username }}" data-email="{{ $user->email }}" data-designation="{{ $user->designation }}">Edit</button>
-                                            <button class="btn btn-raised btn-danger" id="deleteUserModal" data-id="{{ $user->id }}">Remove</button>
+                                            <button class="btn btn-raised btn-danger" id="deleteUserModal" data-toggle="modal" data-target="#deleteModal" data-id="{{ $user->id }}">Remove</button>
                                         </td>
                                     </tr>
                                 @endforeach
@@ -158,6 +158,27 @@
                 </div>
                 <div class="modal-footer">
                     <button class="btn btn-danger btn-raised" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Delete User Modal -->
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Delete User Confirmation</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                       <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Are you sure you want to remove ?</p>
+                    <input type="hidden" name="del_id"/>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-success btn-raised" id="delete">Remove</button>
                 </div>
             </div>
         </div>
