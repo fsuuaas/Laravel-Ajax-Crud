893e03530cd1df7031a4ad33ee1d1bfa7f3f9f8d
Asif Khan <asifkhan.github@gmail.com>
2019-02-09 03:53:10 +0600

    validation added and bug fixed

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -38,9 +38,22 @@ class UsersController extends Controller {
 
     // update user according to the id
     public function update(Request $request){
-        $user=User::findOrFail($request->id);
-        $user->update($request->all());
-        return response()->json($user);
+        $validators=Validator::make($request->all(),[
+            'username'=>['required',Rule::unique('users')->ignore($request->id)],
+            'email'=>['required','email',Rule::unique('users')->ignore($request->id)],
+            'designation'=>'required'
+        ]);
+        if($validators->fails()){
+            return Response::json(['errors'=>$validators->getMessageBag()->toArray()]);
+        }else{
+            $user=User::findOrFail($request->id);
+            $user->username=$request->username;
+            $user->email=$request->email;
+            $user->designation=$request->designation;
+            $user->password=bcrypt('password');
+            $user->save();
+            return response()->json($user);
+        }
     }
 
     // remove user from the database
